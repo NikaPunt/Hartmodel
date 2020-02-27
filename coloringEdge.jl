bdbdf698f242fbea8f496441cd0ab1c89eb4de8e
KurtBosmans <niels.bertier@student.kuleuven.be>
2020-02-27 16:09:04 +0100

    coloringEdge

---
+++ coloringEdge.jl
@@ -0,0 +1,34 @@
+function coloringEdge(celAutom::CellulaireAutomaat)
+
+    #ne = number of edges
+    colors = [colorant"white" for i in 1:ne(celAutom.mg)]
+
+    j = 0
+    for edge in collect(edges(celAutom.mg))
+        j += 1
+        prop = get_prop(celAutom.mg,edge,:htransition)
+        CV = get_prop(celAutom.mg,edge.dst,:CV)
+        dx = get_prop(celAutom.mg,edge,:dx)
+        if (ltransition + htransition)/dx == 0
+            colors[j]= colorant"white"
+        end
+        elif (ltransition + htransition)/dx < 0.2
+            colors[j]= colorant"grey"
+        end
+        elif (ltransition + htransition)/dx < 0.4
+            colors[j]= colorant"blue"
+        end
+        elif (ltransition + htransition)/dx < 0.6
+            colors[j]= colorant"green"
+        end
+        elif (ltransition + htransition)/dx < 0.8
+            colors[j]= colorant"orange"
+        end
+        elif (ltransition + htransition)/dx == 1
+            colors[j]= colorant"red"
+        end
+
+    end
+
+    return colors = colors
+end
